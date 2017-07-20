ee43706d1159abf7312a83c9a7aa8ba7e1377224
Paweł Adamski <echobanda@gmail.com>
2017-07-20 14:41:59 +0200

    added footer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+// ===== Slider ====
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
 });
 
-console.log('bujaj');
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('.return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('.return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('.return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
